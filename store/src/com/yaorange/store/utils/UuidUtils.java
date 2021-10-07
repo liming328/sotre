26f882f0ff77cc38bbb2a57c1075e41b1b98f0ca
lm <5959525757@qq.com>
2021-10-07 18:11:46 +0800

    10-7

--- store/src/com/yaorange/store/utils/UuidUtils.java
+++
@@ -1,9 +0,0 @@
-package com.yaorange.store.utils;
-
-import java.util.UUID;
-
-public class UuidUtils {
-    public static String getUUID() {
-        return UUID.randomUUID().toString().replaceAll("-","");
-    }
-}
